f8f393b3d2dcf802e35835c4c2180bd7980b8c09
Michael McHenry <michael.e.mchenry4.ctr@mail.mil>
2020-12-31 13:58:59 -0500

    first commit

---
+++ src/main/java/webproject/teboraw/stracker/SkillTrackerApplication.java
@@ -0,0 +1,13 @@
+package webproject.teboraw.stracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/webproject/teboraw/stracker/SkillTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package webproject.teboraw.stracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
